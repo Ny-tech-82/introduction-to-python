354d6c715d52db17a75abd2e001ab62594507e6e
Rose Muthoni <rosenyambura49@gmail.com>
2024-11-10 13:00:03 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input ("what is your name?")
+age = input ("How old are you?")
+location = input ("Where are you from?")
+
+print(f"Hello {'Rose'} , you are {29} years old and live in {'Kenya'}")
